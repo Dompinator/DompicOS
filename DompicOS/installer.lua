03600fef10cf3f2b0437f144f88c9ae5df2bdc85
Dompinator <dompinator@outlook.com>
2026-07-31 13:28:15 +0200

    Update installer.lua

--- DompicOS/installer.lua
+++ DompicOS/installer.lua
@@ -7,83 +7,58 @@ print("DompicOS Installer")
 print("")
 sleep(1)
 
-print("Downloading DompicOS package...")
+local base = "https://raw.githubusercontent.com/Dompinator/DompicOS/refs/heads/main/DompicOS/dompicos_package/"
+
+print("Downloading file list...")
 print("")
 
-fs.makeDir("dompicos_package")
+-- Download manifest
+shell.run("wget " .. base .. "manifest.lua manifest.lua")
 
-shell.run("wget https://raw.githubusercontent.com/Dompinator/DompicOS/refs/heads/main/DompicOS/dompicos_package/startup.lua dompicos_package/startup.lua")
-
-print("Download complete!")
-sleep(1)
-
-local package = "dompicos_package"
-
-if not fs.exists(package) then
-    print("ERROR: dompicos_package missing!")
+if not fs.exists("manifest.lua") then
+    print("ERROR: Could not download manifest.lua!")
     return
 end
 
-local function copyFile(from, to)
-    local input = fs.open(from, "rb")
-    local output = fs.open(to, "wb")
+local files = dofile("manifest.lua")
 
-    output.write(input.readAll())
-
-    input.close()
-    output.close()
-end
-
-local function copyFolder(from, to)
-    if not fs.exists(to) then
-        fs.makeDir(to)
-    end
-
-    for _, file in ipairs(fs.list(from)) do
-        local oldPath = from .. "/" .. file
-        local newPath = to .. "/" .. file
-
-        if fs.isDir(oldPath) then
-            copyFolder(oldPath, newPath)
-        else
-            copyFile(oldPath, newPath)
-        end
-    end
-end
-
-
+print("")
 print("Installing DompicOS...")
 print("")
 
-local items = {
-    "startup.lua",
-    "apps",
-    "system",
-    "websites",
-    "images",
-    "sounds"
-}
+-- Download every file from manifest
+for _, file in ipairs(files) do
 
-for _, item in ipairs(items) do
-    local source = package .. "/" .. item
+    local folder = fs.getDir(file)
 
-    if fs.exists(source) then
-        if fs.isDir(source) then
-            copyFolder(source, item)
-        else
-            copyFile(source, item)
-        end
-
-        print("[OK] " .. item)
+    if folder ~= "" and not fs.exists(folder) then
+        fs.makeDir(folder)
     end
 
-    sleep(0.3)
+    print("Downloading: " .. file)
+
+    shell.run(
+        "wget " ..
+        base .. file ..
+        " " .. file
+    )
+
+    if fs.exists(file) then
+        print("[OK] " .. file)
+    else
+        print("[FAILED] " .. file)
+    end
+
+    sleep(0.2)
 end
 
 
 print("")
 print("Installation complete!")
 print("Restarting in 3 seconds...")
+
+-- Remove temporary manifest
+fs.delete("manifest.lua")
 
 sleep(3)
 
