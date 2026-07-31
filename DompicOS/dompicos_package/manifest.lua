398f64bc60f43f3b35608112de575dff1a727eb6
Dompinator <dompinator@outlook.com>
2026-07-31 13:34:09 +0200

    Create manifest.lua

---
+++ DompicOS/dompicos_package/manifest.lua
@@ -0,0 +1,30 @@
+return {
+    "startup.lua",
+
+    "apps/browser.lua",
+    "apps/settings.lua",
+    "apps/explorer.lua",
+    "apps/delivery.lua",
+    "apps/displays.lua",
+    "apps/dompic_kommun_service.lua",
+
+    "system/settings.lua",
+    "system/back.lua",
+    "system/appLauncher.lua",
+    "system/boot_animation.lua",
+    "system/browser_history.cfg",
+    "system/display.cfg",
+    "system/notifications.lua",
+    "system/notify.lua",
+    "
+
+    "Sounds/startup_sound.dfpwm",
+
+    "images/dfe_logo.nfp",
+
+    "websites/dompic.home.lua",
+    "websites/dompic.cursed.lua",
+    "websites/dompic.cursed.home.lua",
+    "websites/dompic.dfe.lua",
+    "websites/dompic.watcg.lua"
+}
